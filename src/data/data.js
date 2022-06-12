47ef31d1f528fd7a4f8b8ba965a6dff634b94659
WalkxCode <walkxnl@gmail.com>
2022-06-12 15:40:16 +0200

    :fire: Removes unused starter code

--- src/data/data.js
+++ src/data/data.js
@@ -2,8 +2,6 @@ export default [
   {
     id: 1,
     title: "Life Lessons with Katie Zaferes",
-    description:
-      'I will share with you what I call "Positively Impactful Moments of Disappointment." Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.',
     price: 136,
     coverImg: "katie-zaferes",
     stats: {
@@ -16,8 +14,6 @@ export default [
   {
     id: 2,
     title: "Learn Wedding Photography",
-    description:
-      "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
     price: 125,
     coverImg: "wedding",
     stats: {
@@ -30,8 +26,6 @@ export default [
   {
     id: 3,
     title: "Group Mountain Biking",
-    description:
-      "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
     price: 50,
     coverImg: "mountain-bike",
     stats: {
@@ -44,8 +38,6 @@ export default [
   {
     id: 4,
     title: "Learn architectural design, explained by an expert",
-    description:
-      "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
     price: 200,
     coverImg: "architechture",
     stats: {
@@ -58,8 +50,6 @@ export default [
   {
     id: 5,
     title: "Help clean the beach with a non-profit organization",
-    description:
-      "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
     price: 0,
     coverImg: "beach",
     stats: {
@@ -72,8 +62,6 @@ export default [
   {
     id: 6,
     title: "Experience your city from above",
-    description:
-      "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
     price: 28,
     coverImg: "city",
     stats: {
